fbcb1d1f2f7c8a083d0ef5fd49cda50bb137debf
hhlin <smartken0824@gmail.com>
2014-03-31 09:20:48 +0800

    Update spring.js
    脚本注入

--- spring.js
+++ spring.js
@@ -3,3 +3,24 @@ $.extend(spring.config, {
     {name: 'jeasyui design', number: 1},
   ]
 });
+
+
+var method='/repos/' + spring.config.owner + '/' + spring.config.repo;
+
+spring.fn.request(method,{},function(data){
+	      var root=data.data;
+	      var desc=root["description"];
+		  var owner=root["owner"];
+		  var login=owner["login"];
+		  var owner_home=owner["html_url"];
+		  var avatar_url=owner["avatar_url"];
+		  var full_name=root["full_name"];
+		  var a_owner_home="<a target='_blank' href='"+owner_home+"'  >"+login+"</a>";
+		  
+		  var conf_desc="<img src='"+avatar_url+"' style='width:46px'  />&nbsp;&nbsp;"+a_owner_home+"<br/>"+desc+"<br/>"+spring_desc;
+		  document.title=full_name+":"+desc;
+		  $.extend(spring.config, {
+		    desc:conf_desc
+		  });
+//$(".desc").text(desc);
+	});
